279bb11d32c5b3eceec4cba5c34d828cf5e72f1a
Jonathan Lima <jonathanhenriquecwb@gmail.com>
2019-11-07 18:53:08 -0300

    Buscar CEP Web Sevices viacep

--- PizzariaWeb/Controllers/UsuarioController.cs
+++ PizzariaWeb/Controllers/UsuarioController.cs
@@ -1,20 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using Domain.Models;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using Repository.DAL;
 
 namespace PizzariaWeb.Controllers
 {
     public class UsuarioController : Controller
     {
+        #region Confi
         private readonly UsuarioDAO _usuarioDAO;
         public UsuarioController(UsuarioDAO usuarioDAO)
         {
             _usuarioDAO = usuarioDAO;
         }
+        #endregion
         #region INDEX LISTAR
         public IActionResult Index()
         {
@@ -24,7 +28,16 @@ namespace PizzariaWeb.Controllers
         #region CADASTRAR
         public IActionResult Cadastrar()
         {
-            return View();
+            Usuario u = new Usuario();
+            if (TempData["Endereco"] != null)
+            {
+                string resultado = TempData["Endereco"].ToString();
+                Endereco endereco =
+                    JsonConvert.DeserializeObject<Endereco>
+                    (resultado);
+                u.Endereco = endereco;
+            }
+            return View(u);
         }
         [HttpPost]
         public IActionResult Cadastrar(Usuario usuario)
@@ -38,6 +51,16 @@ namespace PizzariaWeb.Controllers
                 ModelState.AddModelError("", "Este e-mail já está sendo utilizado!");
             }
             return View(usuario);
+        }
+        #endregion
+        #region BUSCAR CEP
+        [HttpPost]
+        public IActionResult BuscarCep(Usuario u)
+        {
+            string url = "https://viacep.com.br/ws/" + u.Endereco.Cep + "/json/";
+            WebClient client = new WebClient();
+            TempData["Endereco"] = client.DownloadString(url);
+            return RedirectToAction(nameof(Cadastrar));
         }
         #endregion
     }
