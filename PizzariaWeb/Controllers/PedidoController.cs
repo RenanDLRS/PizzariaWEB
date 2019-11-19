b0593cfe2930324f024682080a0f9cc9d34e452d
RenanDLRS <renanlarosa@hotmail.com>
2019-11-18 22:44:19 -0300

    Controller Pedido Inicio

---
+++ PizzariaWeb/Controllers/PedidoController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Repository.DAL;
+
+namespace PizzariaWeb.Controllers
+{
+    public class PedidoController : Controller
+    {
+        #region DAO configuração
+        private readonly TamanhoDAO _tamanhoDAO;
+        private readonly SaborDAO _saborDAO;
+        private readonly BebidaDAO _bebidaDAO;
+        public PedidoController(TamanhoDAO tamanhoDAO, SaborDAO saborDAO, BebidaDAO bebidaDAO)
+        {
+            _tamanhoDAO = tamanhoDAO;
+            _saborDAO = saborDAO;
+            _bebidaDAO = bebidaDAO;
+        }
+        #endregion
+
+        public IActionResult Index()
+        {
+            ViewBag.ListaTamanho = _tamanhoDAO.Listar();
+            ViewBag.ListaSabor = _saborDAO.Listar();
+            ViewBag.ListaBebida = _bebidaDAO.Listar();
+            return View();
+        }
+    }
+}
